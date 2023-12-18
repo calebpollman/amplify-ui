70515e2885087000928a614bb2191bd922714b2d
Caleb Pollman <cpollman@amazon.com>
2023-12-18 14:43:04 -0800

    delete testEnviroment

--- docs/jest.config.ts
+++ docs/jest.config.ts
@@ -4,7 +4,7 @@ const jestConfig: Config = {
   setupFiles: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleDirectories: ['node_modules', 'src'],
-  testEnvironment: 'jsdom',
+  // testEnvironment: 'jsdom',
 };
 
 export default jestConfig;
